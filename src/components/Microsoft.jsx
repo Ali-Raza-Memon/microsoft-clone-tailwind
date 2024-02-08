21dc629292327be0d273b32bb59025f081cdbb3a
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2024-02-09 02:16:13 +0500

    half completed responsiveness navbar

--- src/components/Microsoft.jsx
+++ src/components/Microsoft.jsx
@@ -14,10 +14,20 @@ const Microsoft = () => {
           <div className="search md:hidden">SearchIcon</div>
         </div>
 
-        <div className="logo text-center md:order-1">Microsoft</div>
+        <div className="logo text-center flex md:order-1">
+            <div>Microsoft</div>
+            <div className="features absolute w-fit md:w-auto md:flex md:bg-white  bg-gray-200 inset-0 md:mx-4  md:space-x-2 -translate-x-96 md:translate-x-0">
+                <div className="fitem">Microsoft 365</div>
+                <div className="fitem">Office</div>
+                <div className="fitem">Windows</div>
+                <div className="fitem">Surface</div>
+                <div className="fitem">Xbox</div>
+                <div className="fitem">Support</div>
+            </div>    
+        </div>
         <div className="cart text-center md:order-3 flex">
             
-        <div className="search hidden md:block">SearchIcon</div>
+        <div className="search hidden md:block">All Microsoft SearchIcon</div>
             
             <div>Cart Account</div></div>
       </div>

92545c041959b7045dc0f5e8937b9e12c5526773
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2024-02-08 22:01:08 +0500

    updated

--- src/components/Microsoft.jsx
+++ src/components/Microsoft.jsx
@@ -5,11 +5,29 @@ const Microsoft = () => {
     <div>
 
         <div className="navbar flex justify-between items-center p-4">
-            <div className="hamburger">
+            
+            <div className='flex justify-center items-center'>
+
+           
+            
+            
+            <div className="hamburger inline-block p-4 cursor-pointer">
                 <div className="line h-0.5 w-6 my-1 bg-black "></div>
                 <div className="line h-0.5 w-6 my-1 bg-black "></div>
                 <div className="line h-0.5 w-6 my-1 bg-black "></div>
             </div>
+
+            <div className="search">
+                
+                SearchIcon
+
+            </div>
+
+            </div>
+
+            <div className="logo text-center">Microsoft</div>
+            <div className="cart text-center">Cart Account</div>
+
         </div>
 
     </div>
